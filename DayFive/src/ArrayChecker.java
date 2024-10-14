36bc924f6484180249bb3329991f44cd26a5944d
20191983 <55544233+20191983@users.noreply.github.com>
2024-10-14 17:24:32 +0100

    dont know

--- DayFive/src/ArrayChecker.java
+++ DayFive/src/ArrayChecker.java
@@ -2,7 +2,7 @@ public class ArrayChecker {
     public static boolean isSymmetrical(int[] arr) {
         if(arr.length % 2 == 0){
             for(int i = 0; i < (arr.length/2) ; i++){
-                if(arr[i] != arr[(i * -1) -1]){
+                if(arr[i] != arr[(i * -1) -2]){
                     return false;
                 }
             }
