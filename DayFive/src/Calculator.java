ad06e4ed098c61aff8d43b6c93f7c587c3af4768
20191983 <55544233+20191983@users.noreply.github.com>
2024-10-14 15:17:55 +0100

    a class that will add, subtract, multiply, divide and tell you the remainder. Returns double

---
+++ DayFive/src/Calculator.java
@@ -0,0 +1,23 @@
+public class Calculator {
+
+    public double add(int numOne, int numTwo) {
+        return numOne + numTwo;
+    }
+
+    public double subtract(int numOne, int numTwo) {
+        return numOne - numTwo;
+    }
+
+    public double multiply(int numOne, int numTwo) {
+        return numOne * numTwo;
+    }
+
+    public double divide(int numOne, int numTwo) {
+        return (double) numOne / numTwo;
+    }
+
+    public double modulus(int numOne, int numTwo) {
+        return numOne % numTwo;
+    }
+
+}
