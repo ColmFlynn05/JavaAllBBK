98919af16f2eece4e732d7e83604e59215614b19
ColmFlynn <58558510+ColmFlynn@users.noreply.github.com>
2024-10-18 20:52:39 +0100

    to many counters, just have an array and incrament each place number in the array. add all together for the total

--- DaySix/src/Main.java
+++ DaySix/src/Main.java
@@ -1,6 +1,41 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        studentGrades();
     }
+    public static void studentGrades() {
+        Scanner scanner = new Scanner(System.in);
+        int input = 0;
+        int numStudents =0;
+        int numFail = 0;
+        int numPass =0;
+        int numMerit = 0;
+        int numDistinction = 0;
+        int numErrors = 0;
+        do {
+            System.out.println("Enter grades -1 to end");
+            input = scanner.nextInt();
+            numStudents++;
+            if(input<0 || input>100){
+                numErrors++;
+            }
+            else if(input < 40){
+                numFail++;
+            }
+            else if(input < 60){
+                numPass++;
+            }
+            else if(input < 80){
+                numMerit++;
+            }
+            else{
+                numDistinction++;
+            }
 
+        }
+        while (input != -1);
+        System.out.println("Number of Students " + (numStudents-numErrors) + ". Number of Fails " + numFail + ". Number of Passes " + numPass + ". Number of merits " + numMerit + ". Number of distinctions " + numDistinction);
+        System.out.println("errors encountered "+ numErrors);
+    }
 }
