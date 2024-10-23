868e6ed31417a74a5bd97c682c7e560839ca4dd9
20191983 <55544233+20191983@users.noreply.github.com>
2024-10-23 15:33:03 +0100

    don't think I changed much just added some test.

---
+++ DayEight/src/MobilePhone.java
@@ -0,0 +1,39 @@
+/**
+ * A class that extends @OldPhone
+ * can play games and has a ring alarm (ringtone is what I think he means)
+ * can print last 10 called numbers.
+ */
+public class MobilePhone extends OldPhone{
+    private final int maxNumSaved = 10;
+    private final String[] lastNumbers = new String[maxNumSaved];
+
+
+    /**
+     * keeps track of numbers for @printLastNumbers
+     * @param number the phone number to call
+     */
+    @Override
+    public void call(String number) {
+        super.call(number);
+        String movedNumber;
+        String savedNumber = lastNumbers[0];
+
+        for (int i = 1; i < maxNumSaved; i++) {
+            movedNumber = lastNumbers[i];
+            lastNumbers[i] = savedNumber;
+            savedNumber = movedNumber;
+        }
+        lastNumbers[0] = number;
+    }
+
+    public void ringAlarm(String alarm){
+        System.out.println(alarm);
+    }
+    public void playGame(String game){
+        System.out.println("Playing " +game);
+    }
+    public void printLastNumbers(){
+        System.out.println("Printing numbers.....");
+        for (String lastNumber : lastNumbers) System.out.println(lastNumber);
+    }
+}
---
+++ DayEight/src/SmartPhone.java
@@ -0,0 +1,15 @@
+/**
+ *
+ */
+public class SmartPhone extends MobilePhone {
+
+/*
+    public void browseWeb(String website){
+        System.out.println("searching " + website);
+    }
+*/
+public String findPosition(){
+        return "Location is......";
+    }
+
+}
