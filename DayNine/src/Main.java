c781e6903dc3cac68cfd3900773c90ce89e8167d
20191983 <55544233+20191983@users.noreply.github.com>
2024-10-28 14:13:33 +0000

    refactored question one according to the ide

--- DayNine/src/Main.java
+++ DayNine/src/Main.java
@@ -2,18 +2,15 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-/*
-        HashSet<String> test = new HashSet<>();
-        LinkedHashSet<String> test2 = new LinkedHashSet<>();
-        TreeSet<String> test3 = new TreeSet<>();
+
         String[] name = {"Java", "C", "PHP", "JavaScript", "Python", "Rust", "Haskell", "Go", "Prolog"};
-        test.addAll(Arrays.asList(name));
-        test2.addAll(Arrays.asList(name));
-        test3.addAll(Arrays.asList(name));
+        HashSet<String> test = new HashSet<>(Arrays.asList(name));
+        LinkedHashSet<String> test2 = new LinkedHashSet<>(Arrays.asList(name));
+        TreeSet<String> test3 = new TreeSet<>(Arrays.asList(name));
         System.out.println(test);
         System.out.println(test2);
         System.out.println(test3);
-*/
+
 
 
     }
