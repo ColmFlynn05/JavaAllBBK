493943aff61eb5e1ef21aabf34808639dc5337a3
20191983 <55544233+20191983@users.noreply.github.com>
2024-10-28 14:09:35 +0000

    day 9 first question and worksheet

---
+++ DayNine/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+/*
+        HashSet<String> test = new HashSet<>();
+        LinkedHashSet<String> test2 = new LinkedHashSet<>();
+        TreeSet<String> test3 = new TreeSet<>();
+        String[] name = {"Java", "C", "PHP", "JavaScript", "Python", "Rust", "Haskell", "Go", "Prolog"};
+        test.addAll(Arrays.asList(name));
+        test2.addAll(Arrays.asList(name));
+        test3.addAll(Arrays.asList(name));
+        System.out.println(test);
+        System.out.println(test2);
+        System.out.println(test3);
+*/
+
+
+    }
+}
