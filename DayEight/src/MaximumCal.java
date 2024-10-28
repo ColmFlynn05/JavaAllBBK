c2517fc2ccc2f97f535fd062e02398e27442c589
20191983 <55544233+20191983@users.noreply.github.com>
2024-10-28 10:46:14 +0000

    fixed with generics

--- DayEight/src/MaximumCal.java
+++ DayEight/src/MaximumCal.java
@@ -1,26 +1,23 @@
 public class MaximumCal {
 
     public int getMax(int n, int m) {
-        if (n > m) {
-            return n;
-        }
-        else {
-            return m;
-        }
+        return checkIsGreater(n, n,n,m);
     }
 
     public Object[] getMax(Object[] o1, Object[] o2) {
-        if (o1.length > o2.length) {
-            return o1;
-        }
-        else {
-            return o2;
-        }
+       return checkIsGreater(o1, o2, o1.length, o2.length);
     }
 
     public String getMax(String number1, String number2) {
         int n1 = Integer.parseInt(number1);
         int n2 = Integer.parseInt(number2);
-        return String.valueOf(getMax(n1, n2));
+        return checkIsGreater(number1,number2,n1,n2);
+    }
+    public static <T> T checkIsGreater(T first, T second, int o1, int o2) {
+        if(o1>o2) {
+            return first;
+        } else{
+            return second;
+        }
     }
 }
