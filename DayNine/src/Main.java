53b1dd47542e4440c1563ddb6e7b683115e589c0
20191983 <55544233+20191983@users.noreply.github.com>
2024-10-30 11:20:44 +0000

    day 10

--- DayNine/src/Main.java
+++ DayNine/src/Main.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-        String[] name = {"Java", "C", "PHP", "JavaScript", "Python", "Rust", "Haskell", "Go", "Prolog"};
+        String[] name = {"Java", "Java","C", "PHP", "JavaScript", "Python", "Rust", "Haskell", "Go", "Prolog"};
         HashSet<String> test = new HashSet<>(Arrays.asList(name));
         LinkedHashSet<String> test2 = new LinkedHashSet<>(Arrays.asList(name));
         TreeSet<String> test3 = new TreeSet<>(Arrays.asList(name));
